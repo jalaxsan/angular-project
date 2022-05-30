0265320c0ad0947069bd3ab996834a7c200bc5ab
jalaxsan <alaxsan.jeya@gmail.com>
2022-05-30 10:40:46 +0200

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 
+// @NgModule({
+//   declarations: [
+//     AppComponent,
+//     TopBarComponent,
+//     ProductListComponent,
+//     ProductAlertsComponent,
+//   ],
+
 @NgModule({
   imports: [
     BrowserModule,
--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -2,21 +2,11 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../products';
 
 export class ProductAlertsComponent implements OnInit {
+
   @Input() product!: Product;
   constructor() { }
 
-  ngOnInit(){
-
+  ngOnInit() {
   }
-}
 
-@Component({
-  selector: 'app-product-alerts',
-  templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.css'],
-})
-export class ProductAlertsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
 }
